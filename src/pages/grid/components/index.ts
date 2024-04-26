c9c54712ed0f927aac2365c90d9715daf55d3898
Dam <60846068+j2h30728@users.noreply.github.com>
2024-04-26 17:54:17 +0900

    feat: verification for grid 중 uploader grid 페이지 view 연결 [#75]

--- src/pages/grid/components/index.ts
+++ src/pages/grid/components/index.ts
@@ -1,5 +1,5 @@
-export { default as AllGrid } from './AllGrid';
-export { default as UploaderGrid } from './UploaderGrid';
+export { default as AllTypeGrid } from './AllTypeGrid';
+export { default as UploaderTypeGrid } from './UploaderTypeGrid';
 
 export { default as SortButton } from './SortButton';
 export { default as GridLayout } from './GridLayout';
