a950964d40b4fa5de22290aa48ccc979cd04431e
Dam <60846068+j2h30728@users.noreply.github.com>
2024-04-26 17:53:07 +0900

    feat: verification for grid 중 grid페이지 view 연결 [#75]

--- src/pages/grid/Grid.tsx
+++ src/pages/grid/Grid.tsx
@@ -1,19 +1,14 @@
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 import ROUTE_PATH from '../../router/constants';
 import { Layout } from '../../components';
 import { SlideIcon } from '../../components/Icons';
-import { AllGrid, GridLayout, UploaderGrid } from './components';
-
-export type Grid = 'all' | 'uploader';
-export type Sort = 'descending' | 'descending';
+import { AllTypeGrid, GridLayout, UploaderTypeGrid } from './components';
+import { GridType } from '../../types/verification';
 
 const Grid = () => {
   const navigate = useNavigate();
-  const { type } = useParams<{ type: Grid }>();
-  const { search } = useLocation();
-  const searchParams = new URLSearchParams(search);
-  const sortType = searchParams.get('sortType') || 'ascending';
+  const { type } = useParams<{ type: GridType }>();
 
   return (
     <Layout
@@ -22,9 +17,10 @@ const Grid = () => {
         rightButton: <SlideIcon onClick={() => navigate(ROUTE_PATH.SLIDE)} />,
       }}
     >
-      <GridLayout type={type ?? 'all'} />
-      {type === 'all' && <AllGrid sortType={sortType as Sort} />}
-      {type === 'uploader' && <UploaderGrid />}
+      <GridLayout type={type ?? 'all'}>
+        {type === 'all' && <AllTypeGrid />}
+        {type === 'uploader' && <UploaderTypeGrid />}
+      </GridLayout>
     </Layout>
   );
 };
