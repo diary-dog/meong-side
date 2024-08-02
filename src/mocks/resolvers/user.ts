1ff8232f8d024d3b2bea95289d3c37fe36d69abb
Dam <60846068+j2h30728@users.noreply.github.com>
2024-08-02 17:01:09 +0900

    chore: 카카오 로그인 mock api 수정[#90]

--- src/mocks/resolvers/user.ts
+++ src/mocks/resolvers/user.ts
@@ -1,48 +1,15 @@
 import { HttpResponse, delay } from 'msw';
-import axios from 'axios';
-import qs from 'qs';
 
 import { MSWResolvers } from '../../shared/lib/mswUtils';
 
-const {
-  VITE_KAKAO_REST_API_KEY,
-  VITE_KAKAO_REDIRECT_URI,
-  VITE_KAKAO_CLIENT_SECRET,
-} = import.meta.env;
-
-interface ResponseKakaoLogin {
-  access_token: string;
-  expires_in: number;
-  refresh_token: string;
-  refresh_token_expires_in: number;
-  scope: string;
-  token_type: string;
-}
-
 export const user = {
-  getFamilyMembers: async ({ request }) => {
+  kakaoLogin: async () => {
     await delay();
-    const queryString = new URL(request.url).search;
-    const parsedQuery = qs.parse(queryString, { ignoreQueryPrefix: true });
-
-    const payload = qs.stringify({
-      grant_type: 'authorization_code',
-      client_id: VITE_KAKAO_REST_API_KEY,
-      redirect_uri: VITE_KAKAO_REDIRECT_URI,
-      code: parsedQuery.code,
-      client_secret: VITE_KAKAO_CLIENT_SECRET,
-    });
-
     try {
-      const { data } = await axios.post<ResponseKakaoLogin>(
-        'https://kauth.kakao.com/oauth/token',
-        payload
-      );
-
       // mocking 을 위하여 HttpOnly; Secure 미추가
-      const cookie = `access_token=${data.access_token}; Path=/; Max-Age=${data.expires_in};`;
+      const cookie = `access_token=access-token; Path=/; Max-Age=604800;`;
       return HttpResponse.json(
-        { data },
+        { data: '로그인 성공' },
         {
           headers: {
             'Set-Cookie': cookie,
