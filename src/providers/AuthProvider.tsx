6b46edbf381cb1cefd4b7cd3174798c5f0f24bfb
Dam <60846068+j2h30728@users.noreply.github.com>
2024-08-07 16:22:16 +0900

    feat: auth provider 범위 변경[#92]

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -2,10 +2,12 @@ import { Navigate, Outlet } from 'react-router-dom';
 
 import ROUTE_PATH from '../shared/constants/routePath';
 
-const AuthLayout = () => {
+const AuthProvider = () => {
   if (!localStorage.getItem('token')) return <Navigate to={ROUTE_PATH.INTRO} />;
+
+  // 펫 등록 여부에 따라서 다르게 리디렉션 해줄 것
 
   return <Outlet />;
 };
 
-export default AuthLayout;
+export default AuthProvider;
