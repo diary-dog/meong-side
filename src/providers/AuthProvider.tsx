c0965ad3a64f6f992ca32a6c2296e447451bf05c
Dam <60846068+j2h30728@users.noreply.github.com>
2024-08-07 16:45:56 +0900

    feat: token storage 생성[#92]

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,9 +1,10 @@
 import { Navigate, Outlet } from 'react-router-dom';
 
 import ROUTE_PATH from '../shared/constants/routePath';
+import { tokenStorage } from '../shared/lib/tokenStorage';
 
 const AuthProvider = () => {
-  if (!localStorage.getItem('token')) return <Navigate to={ROUTE_PATH.INTRO} />;
+  if (!tokenStorage.getToken()) return <Navigate to={ROUTE_PATH.INTRO} />;
 
   // 펫 등록 여부에 따라서 다르게 리디렉션 해줄 것
 
