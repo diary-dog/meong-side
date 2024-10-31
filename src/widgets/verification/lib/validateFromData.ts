fcb26fef2975e3403da1ab909c8e06e2397cf5eb
Dam <60846068+j2h30728@users.noreply.github.com>
2024-10-31 18:57:13 +0900

    feat: walk option을 채우지 않을 경우 등록 확인 페이지에서 산책 시간 내용 미표시[#99]

--- src/widgets/verification/lib/validateFromData.ts
+++ src/widgets/verification/lib/validateFromData.ts
@@ -3,16 +3,15 @@ import { UploadVerificationForm } from '../../../shared/types/verification';
 export default function isValidUploadFormData(
   formData: UploadVerificationForm
 ) {
-  if ('walkDetail' in formData) {
-    return formData.walkDetail?.hours || formData.walkDetail?.minutes;
-  }
-
   if (
     formData.comment ||
     formData.verificationDetail ||
     formData.image.length > 0
   ) {
     return true;
+  }
+  if ('walkDetail' in formData) {
+    return formData.walkDetail?.hours || formData.walkDetail?.minutes;
   }
   return false;
 }
