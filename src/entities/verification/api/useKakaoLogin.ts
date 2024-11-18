31a2bb7eb56a3a0d343077a24f180d6c48312b05
Dam <60846068+j2h30728@users.noreply.github.com>
2024-11-18 20:38:40 +0900

    feat: 카카오 로그인 mutation 함수 생성 [#104]

--- src/entities/verification/api/useKakaoLogin.ts
+++ src/entities/verification/api/useKakaoLogin.ts
@@ -1,10 +1,19 @@
 import { useMutation } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
 
 import userAPI from './userAPI';
+import ROUTE_PATH from '../../../shared/constants/routePath';
+import { tokenStorage } from '../../../shared/lib/tokenStorage';
 
 const useKakaoLogin = () => {
+  const navigate = useNavigate();
+
   return useMutation({
     mutationFn: (code: string) => userAPI.kakaoLogin(code),
+    onSuccess: ({ data }) => {
+      tokenStorage.setToken(data.accessToken);
+      navigate(ROUTE_PATH.ROOT);
+    },
   });
 };
 export default useKakaoLogin;
