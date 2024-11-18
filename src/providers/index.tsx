153a0a1890e489f36480ad973363b00033381eec
Dam <60846068+j2h30728@users.noreply.github.com>
2024-11-18 22:46:01 +0900

    feat: 라우터내 lazy로딩 분리[#78]

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -5,8 +5,8 @@ import { QueryProvider } from './QueryProvider';
 import { ThemeProvider } from './ThemeProvider';
 import ErrorBoundary from '../shared/error/ErrorBoundary';
 import FallbackComponent from '../shared/error/FallbackComponent';
-import router from '../router';
 import Spinner from '../shared/ui/Spinner';
+import router from '@/router';
 
 const Providers = () => {
   return (
